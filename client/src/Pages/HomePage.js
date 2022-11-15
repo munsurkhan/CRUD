c75f488ff58c9fc52684cb77b690683d49fcbdab
Munsur Rahman <munsurinfo@gmail.com>
2022-11-15 13:04:44 +0600

    151122

---
+++ client/src/Pages/HomePage.js
@@ -0,0 +1,13 @@
+import {Component, Fragment} from "react";
+
+class HomePage extends Component{
+    render() {
+        return(
+            <Fragment>
+                <h1>Home Page</h1>
+            </Fragment>
+        )
+    }
+}
+
+export default HomePage;
