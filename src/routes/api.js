8504288a10f3299b49599de0983a56bafce27680
Munsur Rahman <munsurinfo@gmail.com>
2022-11-15 20:10:41 +0600

    9
    9

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,18 @@
 const express =require('express');
+const ProductsController = require("../controllers/ProductsController");
 const router =express.Router();
 
+// C = Create
+router.post("/CreateProduct", ProductsController.CreateProduct);
 
+// R = Read
+router.get("/ReadProduct", ProductsController.ReadProduct);
+
+// U = Update
+router.post("/UpdateProduct/:id", ProductsController.UpdateProduct);
+
+// D = Delete
+router.get("/DeleteProduct/:id", ProductsController.DeleteProduct);
 
 
 module.exports=router;
